d9b1bc0f073154b3f7ea4bf1c6643242aa90db9a
asdfaaewr <phil.seemann@gmail.com>
2024-12-06 21:54:01 +0100

    .

--- day_6.py
+++ day_6.py
@@ -5,7 +5,7 @@ with open('in_' + str(day) + '_test' * test + '.txt') as file:
 for i in range(len(data)):
     for j in range(len(data[-1])):
         if data[i][j] == '^':
-            r, c = i, j 
+            r, c = start = i, j 
 
 v = set([(r, c)])
 d = (-1, 0)
@@ -24,3 +24,29 @@ while True:
         v.add((r, c))
 
 print(len(v))
+
+t = 0
+for p in v:
+    if p == start:
+        continue
+    d = (-1, 0)
+    r, c = start
+    v2 = set([r, c, d])
+
+    while True:            
+        if not 0 <= r + d[0] < len(data):
+            break
+
+        if not 0 <= c + d[1] < len(data[-1]):
+            break
+
+        if data[r + d[0]][c+ d[1]] == '#' or p == (r + d[0], c+ d[1]):
+            d = (d[1], -d[0])
+        else:
+            r, c = r + d[0], c+ d[1]
+            if (r, c, d) in v2:
+                t+=1
+                break
+            v2.add((r, c, d))
+
+print(t)
